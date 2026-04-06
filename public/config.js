c21fed29011aa91d0777c9614a3941fea4c0c74f
Mario Reder <mario.reder@pm.me>
2026-04-07 01:23:19 +0200

    Update DEX configuration and branding

--- public/config.js
+++ public/config.js
@@ -18,7 +18,7 @@ window.__RUNTIME_CONFIG__ = {
   "VITE_APP_DESCRIPTION": "Exchange for seemless trading",
   "VITE_HAS_PRIMARY_LOGO": "true",
   "VITE_HAS_SECONDARY_LOGO": "true",
-  "VITE_ENABLED_MENUS": "Trading,Portfolio,Swap,Markets,Points,Campaigns",
+  "VITE_ENABLED_MENUS": "Trading,Portfolio,Markets,Points",
   "VITE_CUSTOM_MENUS": "",
   "VITE_ENABLE_SERVICE_DISCLAIMER_DIALOG": "false",
   "VITE_ENABLE_CAMPAIGNS": "false",
